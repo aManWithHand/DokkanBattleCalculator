555b29c9b7856bc39bdc7a9773ccf8d7683e5410
aManWithHand <88043496+aManWithHand@users.noreply.github.com>
2025-01-26 23:13:22 +0700

    create base GUI

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import customtkinter
 import dokkan_cal as dk
 
-
-dk.a()
-print(dir())
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-app.geometry("400x240")
+class App(customtkinter.CTk):
+    def __init__(self, fg_color = None, **kwargs):
+        super().__init__(fg_color, **kwargs)
+        self.title("Dokkan Battle Calculator")
+        self.geometry(f"{400}x{600}")
 
-def button_function():
-    print("button pressed")
 
-# Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
-button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
-#customtkinter.deactivate_automatic_dpi_awareness()
-app.mainloop()
+#button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
+#button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
