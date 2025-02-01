cb6d322dabe850bce260a57089fb3bbe0972fe24
aManWithHand <88043496+aManWithHand@users.noreply.github.com>
2025-02-02 06:19:51 +0700

    change layout

--- dokkan_cal/gui/outputFrame.py
+++ dokkan_cal/gui/outputFrame.py
@@ -3,18 +3,18 @@ from ..base import Base
 
 
 class OutputFrame(customtkinter.CTkFrame):
-    def __init__(self, master,text, width = 200, height = 200, corner_radius = None, border_width = None, bg_color = "transparent", fg_color = None, border_color = None, background_corner_colors = None, overwrite_preferred_drawing_method = None, **kwargs):
+    def __init__(self, master,text, width = 150, height = 200, corner_radius = None, border_width = None, bg_color = "transparent", fg_color = None, border_color = None, background_corner_colors = None, overwrite_preferred_drawing_method = None, **kwargs):
         super().__init__(master, width, height, corner_radius, border_width, bg_color, fg_color, border_color, background_corner_colors, overwrite_preferred_drawing_method, **kwargs)
 
-        self.label1 = customtkinter.CTkLabel(self,fg_color="#484c4d",text=text,width=width)
-        self.label1.grid(column=0, row=0, padx=15, pady=(15,5))
+        self.label1 = customtkinter.CTkLabel(self,fg_color="#484c4d",text=text, width=width)
+        self.label1.grid(column=0, row=0,padx=15, pady=(15,5))
 
-        self.label2 = customtkinter.CTkLabel(self,width=width,text="Phase1: 254613")
-        self.label2.grid(column=0, row=1, padx=15, pady=(0,5))
-        self.label3 = customtkinter.CTkLabel(self,width=width,text="Phase2: 254613")
-        self.label3.grid(column=0, row=2, padx=15, pady=(0,5))
-        self.label4 = customtkinter.CTkLabel(self,width=width,text="    SA: 254613")
-        self.label4.grid(column=0, row=3, padx=15, pady=(0,5))
+        self.label2 = customtkinter.CTkLabel(self,text="-")
+        self.label2.grid(column=0, row=1, pady=(0,5))
+        self.label3 = customtkinter.CTkLabel(self,text="-")
+        self.label3.grid(column=0, row=2, pady=(0,5))
+        self.label4 = customtkinter.CTkLabel(self,text="-")
+        self.label4.grid(column=0, row=3, pady=(0,5))
 
     def show(self,base:Base):
         #get data from base and show in OutputFrame
--- main.py
+++ main.py
@@ -9,30 +9,31 @@ class App(customtkinter.CTk):
     def __init__(self, fg_color = None, **kwargs):
         super().__init__(fg_color, **kwargs)
         self.title("Dokkan Battle Calculator")
-        self.geometry(f"{600}x{700}")
-        self.grid_columnconfigure((0,1,3),weight=1)
+        #self.geometry(f"{500}x{700}")
 
 #------------------DEF GUI------------------------------------------------#
-        self.defInput1 = gui.PhaseFrame(self,"PHASE1 (before attack)")
-        self.defInput1.grid_configure(column=1, row=0, pady=15, padx=15)
-        self.defInput2 = gui.PhaseFrame(self,"PHASE2 (attacking)")
-        self.defInput2.grid_configure(column=1, row=1, pady=(0,15))
-        self.defInput3 = gui.LinkFrame(self)
-        self.defInput3.grid_configure(column=1, row=2, pady=(0,15))
+        self.defInput0 = gui.StatusFrame(self)
+        self.defInput0.grid_configure(column=2, row=0, pady=(0,0))
+        self.defInput1 = gui.LeaderFrame(self,values=["1","77", "170","200"])
+        self.defInput1.grid_configure(column=2, row=1, pady=(0,0), padx=(0,15))
+        
+        self.defInput2 = gui.PhaseFrame(self,"PHASE1 (before attack)")
+        self.defInput2.grid_configure(column=1, row=0, pady=15, padx=15)
+        self.defInput3 = gui.PhaseFrame(self,"PHASE2 (attacking)")
+        self.defInput3.grid_configure(column=1, row=1, pady=(0,15))
+        self.defInput4 = gui.LinkFrame(self)
+        self.defInput4.grid_configure(column=1, row=2, pady=(0,15))
 
-        self.defInput4 = gui.LeaderFrame(self,values=["1","77", "170","200"])
-        self.defInput4.grid_configure(column=2, row=1, pady=(0,0))
-        self.defInput5 = gui.StatusFrame(self)
-        self.defInput5.grid_configure(column=2, row=0, pady=(0,0))
+        
         
         #add input for SA might use CTkSegmentedButton
         self.defOutput1 = gui.OutputFrame(self,"DEF")
-        self.defOutput1.grid(column=1,row=4,pady=(15,15))
+        self.defOutput1.grid_configure(column=1,row=4,pady=15)
         self.defButton1 = customtkinter.CTkButton(self,text="calculate",command=self.calDEF)
         self.defButton1.grid(column=1,row=3)
 
 #------------------LOGIC--------------------------------------------------#
-        self.defList = [dkc.Defence(stat=self.defInput5, leader=self.defInput4, phase1=self.defInput1, phase2=self.defInput2, link=self.defInput3, out=self.defOutput1)]
+        self.defList = [dkc.Defence(stat=self.defInput0, leader=self.defInput1, phase1=self.defInput2, phase2=self.defInput3, link=self.defInput4, out=self.defOutput1)]
     
     def calDEF(self):
         self.defList[0].cal()
