b69a8eea6d02dc8d77c01c223df4d5a4cc39e846
aManWithHand <88043496+aManWithHand@users.noreply.github.com>
2025-04-10 13:36:05 +0700

    Update main.py
    adjust code to PEP 8 style

--- main.py
+++ main.py
@@ -9,37 +9,69 @@ class App(customtkinter.CTk):
     def __init__(self, fg_color = None, **kwargs):
         super().__init__(fg_color, **kwargs)
         self.title("Dokkan Battle Calculator")
-        #self.geometry(f"{500}x{700}")
+        
 
 #------------------DEF GUI------------------------------------------------#
 #------------------right hand side----------------------------------------#
-        self.defInput0 = gui.StatusFrame(self)
-        self.defInput0.grid_configure(column=2, row=0, pady=(0,0))
-        self.defInput1 = gui.LeaderFrame(self,values=["1","77", "170","200"])
-        self.defInput1.grid_configure(column=2, row=1, pady=(0,0), padx=(0,15))
-        self.defInput2 = gui.PhaseFrame(self,"PHASE1 (before attack)")
-        self.defInput2.grid_configure(column=2, row=2, pady=15, padx=15)
+        self.status_frame = gui.StatusFrame(self)
+        self.status_frame.grid_configure(column= 2,
+                                         row= 0,
+                                         pady= (0,0))
+        self.leader_frame1 = gui.LeaderFrame(self,
+                                             values= ["1","77", "170","200"])
+        self.leader_frame1.grid_configure(column=2,
+                                          row=1,
+                                          pady=(0,0),
+                                          padx=(0,15))
+        self.phase_frame1 = gui.PhaseFrame(self,
+                                           text= "PHASE1 (before attack)")
+        self.phase_frame1.grid_configure(column=2,
+                                         row=2,
+                                         pady=15,
+                                         padx=15)
 
 #------------------left hand side----------------------------------------#
-        self.defInput4 = gui.LinkFrame(self)
-        self.defInput4.grid_configure(column=1, row=0, pady=(15,15))
-        self.defInput3 = gui.PhaseFrame(self,"PHASE2 (attacking)")
-        self.defInput3.grid_configure(column=1, row=1, pady=(0,15))   
-        self.defInput5 = gui.SuperAttackFrame(self)
-        self.defInput5.grid_configure(column=1, row=2, pady=(0,0), padx=(15,0))
+        self.link_frame = gui.LinkFrame(self)
+        self.link_frame.grid_configure(column=1,
+                                       row=0,
+                                       pady=(15,15))
+        self.phase_frame2 = gui.PhaseFrame(self,
+                                           text="PHASE2 (attacking)")
+        self.phase_frame2.grid_configure(column=1,
+                                         row=1,
+                                         pady=(0,15))   
+        self.super_attack_frame = gui.SuperAttackFrame(self)
+        self.super_attack_frame.grid_configure(column=1,
+                                               row=2,
+                                               pady=(0,0),
+                                               padx=(15,0))
 
 #--------------------------middle----------------------------------------#  
-        self.defOutput1 = gui.OutputFrame(self,"DEF")
-        self.defOutput1.grid_configure(column=1,row=4,pady=15, columnspan=2)
-        self.defButton1 = customtkinter.CTkButton(self,text="calculate",command=self.calDEF)
-        self.defButton1.grid_configure(column=1, row=3, columnspan=2)
+        self.output_frame = gui.OutputFrame(self,
+                                            text="DEF")
+        self.output_frame.grid_configure(column=1,
+                                         row=4,
+                                         pady=15,
+                                         columnspan=2)
+        self.cal_button = customtkinter.CTkButton(self,
+                                                  text="calculate",
+                                                  command=self.calDEF)
+        self.cal_button.grid_configure(column=1,
+                                       row=3,
+                                       columnspan=2)
 
 #------------------LOGIC--------------------------------------------------#
-        self.defCalList = [dkc.Defence(stat=self.defInput0, leader=self.defInput1, phase1=self.defInput2, phase2=self.defInput3, link=self.defInput4, sa=self.defInput5, out=self.defOutput1)]
+        self.data_list = [dkc.Defence(stat=self.status_frame,
+                                       leader=self.leader_frame1,
+                                       phase1=self.phase_frame1,
+                                       phase2=self.phase_frame2,
+                                       link=self.link_frame,
+                                       sa=self.super_attack_frame,
+                                       out=self.output_frame)]
     
     def calDEF(self):
-        self.defCalList[0].cal()
-        self.defCalList[0].show()
+        self.data_list[0].cal()
+        self.data_list[0].show()
         pass
 
 
